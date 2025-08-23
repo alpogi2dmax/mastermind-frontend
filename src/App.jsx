ae379c53a7318119cdbf50da6d809ac887fe7086
Al Cerdan Lico <alcerdan@yahoo.com>
2025-08-23 15:02:06 -0700

    add evaluate guess in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 
 function App() {
   const [secret, setSecret] = useState([])
+  const [guess, setGuess] = useState('')
+  const [result, setResult] = useState(null)
 
   const handleSecret = async () => {
     try {
@@ -17,10 +19,38 @@ function App() {
     }
   }
 
+  const handleGuess = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/evaluate', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ guess: guess.split('').map(Number)} )
+      })
+      const data = await response.json()
+      setResult(data)
+    } catch (error) {
+      console.error('Error evaluating guess:', error)
+      alert('Failed to evaluate guess.')
+    }
+  }
+
   return (
     <main>
       <button onClick={handleSecret}>Start Game</button>
       {secret.length > 0 && <p>Secret Code: {secret.join('')}</p>}
+      {secret.length > 0 && (
+        <>
+          <input
+            value={guess}
+            onChange={e => setGuess(e.target.value)}
+            maxLength={secret.length}
+            placeholder="Enter your guess"
+          />
+          <button onClick={handleGuess}>Submit Guess</button>
+        </>
+      )}
+
+      {result && <p>Correct Number: {result.correct_number} and Correct Location: {result.correct_location}</p>}
     </main>
   )
 }
